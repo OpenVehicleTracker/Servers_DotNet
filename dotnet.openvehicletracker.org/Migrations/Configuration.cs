ad0cff9ba7a959e6fbed1ef1f002ec171186a577
Derrick <derrick@alienseed.com>
2013-10-07 20:03:19 -0400

    Implemented database Seed for initialization of default user admin:admin and Administrators group

--- dotnet.openvehicletracker.org/Migrations/Configuration.cs
+++ dotnet.openvehicletracker.org/Migrations/Configuration.cs
@@ -1,9 +1,12 @@
 namespace dotnet.openvehicletracker.org.Migrations
 {
+    using dotnet.openvehicletracker.org.Models.Entities;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
+    using System.Web.Security;
+    using WebMatrix.WebData;
 
     internal sealed class Configuration : DbMigrationsConfiguration<dotnet.openvehicletracker.org.Models.Entities.OVTContext>
     {
@@ -12,8 +15,34 @@ namespace dotnet.openvehicletracker.org.Migrations
             AutomaticMigrationsEnabled = false;
         }
 
-        protected override void Seed(dotnet.openvehicletracker.org.Models.Entities.OVTContext context)
+
+        protected override void Seed(OVTContext context)
         {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+
+            WebSecurity.InitializeDatabaseConnection(
+                "DefaultConnection",
+                "UserProfile",
+                "UserId",
+                "UserName", autoCreateTables: true);
+
+            if (!Roles.RoleExists("Administrator"))
+                Roles.CreateRole("Administrator");
+
+            if (!WebSecurity.UserExists("admin"))
+                WebSecurity.CreateUserAndAccount(
+                    "admin",
+                    "admin",
+                    new { EmailAddress = "admin@openvehicletracker.org" });
+
+            if (!Roles.GetRolesForUser("admin").Contains("Administrator"))
+                Roles.AddUsersToRoles(new[] { "admin" }, new[] { "Administrator" });
+
+
             //  This method will be called after migrating to the latest version.
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
